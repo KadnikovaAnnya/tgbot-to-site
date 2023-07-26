cf9b6092856f52c7ab3953d9138721a645eb0d47
annya <annya.kkk@yandex.ru>
2023-07-26 12:22:46 +0500

    nastroili git

---
+++ server.js
@@ -0,0 +1,66 @@
+const TelegramBot = require('node-telegram-bot-api')
+const token = '6351354525:AAFS87RsMexK91RBnOgl5WQvOMV6bLJFeGA'
+const bot = new TelegramBot(token, {polling:true})
+
+// Серверная часть
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+// app.get('/', (req, res) => {
+//     res.send('Hello debug_Yourself')
+// })
+
+app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
+
+const commands = [
+    {
+        command:'start',
+        description:'Запустить бота'
+    },
+    {
+        command:'save',
+        description:'Сохранить результат'
+    },
+    {
+        command:'heading',
+        description:'Заголовок'
+    },
+    {
+        command:'img',
+        description:'Отправить изображение'
+    },
+    {
+        command:'p',
+        description:'Параграф'
+    },
+    {
+        command:'adore',
+        description:'похвалить)'
+    }
+]
+bot.setMyCommands(commands)
+
+
+// await bot.sendMessage(msg.chat.id, stroka, {parse_mode:'HTML'})
+
+bot.onText(/\/adore/, async(msg) => {
+    console.log(msg.chat.id)
+    chatId = msg.from.id 
+    try{
+        await bot.sendMessage(msg.chat.id, 'Ты молодец:3')
+    }
+    catch(err){
+        console.error()
+    }
+})
+
+// bot.onText('text', async msg => {
+//     try{
+//         await bot.sendMessage(msg.chat.id, '')
+//     }
+//     catch(err){
+//         console.error()
+//     }
+// })
